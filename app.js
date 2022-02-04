043c1e7850e390c41c716e0582ff924e6c28dc50
bmendoza23 <bmendoz2@uwo.ca>
2022-02-04 18:19:58 -0500

    moved things

--- app.js
+++ app.js
@@ -7,5 +7,9 @@ app.use(express.static('static'));
 app.use(express.urlencoded({
         extended: true
 }));
-
+app.use('/', (req,res) => {
+        console.log('Rendering Home Page');
+        res.status(200).render()
+        console.log('Successfully Rendered');
+})
 app.listen(2000);
