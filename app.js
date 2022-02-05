2dd15962fd6783c51b141eecddfa860d1f1a9745
bmendoza23 <bmendoz2@uwo.ca>
2022-02-04 23:51:48 -0500

    fixed index.js import to index.html

--- app.js
+++ app.js
@@ -9,9 +9,10 @@ app.use(express.static('static'));
 app.use(express.urlencoded({
         extended: true
 }));
-app.use('/', (req,res) => {
+/*app.use('/', (req,res) => {
         console.log('Rendering Home Page');
         res.status(200).render("index");
         console.log('Successfully Rendered');
 })
+*/
 app.listen(2000);
