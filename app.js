db0bd568955bbb9aeef1c86a10df480aab04fc40
bmendoza23 <bmendoz2@uwo.ca>
2022-02-06 13:41:43 -0500

    randomizing teams function + added index.css

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const path = require('path');
-app.set("views", path.join(__dirname,"views"));
+const path = require ('path');
+//app.set("views", path.join(__dirname,"views"));
 
 //Serving Static Contents
 app.use(express.static('static'));
@@ -9,7 +9,7 @@ app.use(express.static('static'));
 app.use(express.urlencoded({
         extended: true
 }));
-/*app.use('/', (req,res) => {
+/*app.get('/', (req,res) => {
         console.log('Rendering Home Page');
         res.status(200).render("index");
         console.log('Successfully Rendered');
--- static/js/index.js
+++ static/js/index.js
@@ -4,6 +4,49 @@ console.log("index.js imported");
 //Global Variables 
 let seriesLength;
 let playerList = [];
+const mwInfoJSON = {
+    "maps":[
+        "arlov_peak",
+        "azhir_cave",
+        "gun_runner",
+        "hackney_yard",
+        "shoot_house",
+        "vacant",
+        "khandor_hideout",
+        "hardhat",
+        "hovec_sawmill"
+    ],
+    "gamemodes":[
+        "snd",
+        "hardpoint",
+        "dom",
+        "ctf"
+    ]
+};// want to use require('../json/mwInfo.json'); but it seems little complicated
+let mapsList = [];
+let gamemodesList = [];
+let team1, team2;
+
+
+//Parses mwInfoJSON object to mapsList and gamemodeList
+function parseMWInfo(){
+    console.log("Parsing MW Info...");
+    //Pushes maps to mapsList
+    for (let map of mwInfoJSON.maps)
+    {
+        mapsList.push(map);
+    }
+    console.log("Maps List: " + mapsList); 
+    //Pushes gamemodes to gamemodesList
+    for (let gamemode of mwInfoJSON.gamemodes)
+    {
+        gamemodesList.push(gamemode);
+    }
+    console.log("Gamemodes List: " + gamemodesList);
+};
+
+
+parseMWInfo();
 
 //Gets series length
 function getSeriesLength() {
@@ -35,7 +78,6 @@ function addPlayer(){
 //Refreshes playerList div
 function refreshPlayerList(){
     $("#playerList").append( "<br>"+ playerList[(playerList.length - 1)]+ "</br>" );
-    
 }
 
 //Adds player to list + refreshes playerList div
@@ -44,22 +86,47 @@ function addPlayerBtnClick(){
     refreshPlayerList();
 }
 
+//Fisher-Yates Shuffle Algorithm
+
+function shuffle(arr) {
+    let currentIndex = arr.length,  randomIndex;
+  
+    // While there are still elements in the array
+    while (currentIndex != 0) { 
+      randomIndex = Math.floor(Math.random() * currentIndex);   //Picking random element
+      currentIndex--;
+  
+      //Swapping index with random index.
+      [arr[currentIndex], arr[randomIndex]] = [arr[randomIndex], arr[currentIndex]];
+    }
+    return array;
+  }
+
 //Randomize Teams
 function randomizeTeams(){
     //Randomize Array
+    shuffle(playerList);
 
     //Split Array into 2
-
-    //Display Array to Page
-}
-
-//Load maps to list
-function loadMaps(){
+    var listHalf = Math.ceil(playerList.length / 2);        //Finding middle point of playerList
+    team1 = playerList.slice(0,listHalf);                   //Slicing first half of list to team1
+    team2 = playerList.slice(listHalf+1, playerList.length);//Slicing second half of list to team2
     
+    //Appending team1 and team2 to Teams div:
+    //Looping over team1 list
+    for (let player in team1){
+         $("#team1").append( "<br>"+ player+ "</br>" ); //Appending player to team1
+    };
+
+    for (let player in team2){
+        $("#team2").append( "<br>"+ player+ "</br>" );
+   }
 }
+
 //Done Button Click
 function done(){ 
     getSeriesLength();
+    randomizeTeams();
 }
 
 
