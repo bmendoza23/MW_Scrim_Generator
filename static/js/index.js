ff7240e391de4fba4d1765ad1c01cda889358fd4
bmendoza23 <bmendoz2@uwo.ca>
2022-02-05 22:09:44 -0500

    added playerlist functionality + select series length functionality

--- static/js/index.js
+++ static/js/index.js
@@ -1,8 +1,65 @@
 //Handles Logic and scripts
-function test(){
-    $('#msg').text("Hello");
-    alert("Button was clicked");
-    console.log("Button Clicked");
+console.log("index.js imported");
+
+//Global Variables 
+let seriesLength;
+let playerList = [];
+
+//Gets series length
+function getSeriesLength() {
+    let seriesLengthInput = String($('input[name=seriesLengthRadio]:checked', '#seriesLengthForm').val());
+    console.log("Series Length Radio Selected: " + seriesLengthInput);
+
+    switch (seriesLengthInput){
+        case '1of1':
+            seriesLength = 1;
+        case '2of3':
+            seriesLength = 3;
+        case '3of5':
+            seriesLength = 5;
+        case '4of7':
+            seriesLength = 7;
+    }
+
+    $('#msg').text("Series Length Selected: " + seriesLength);
+};
+ 
+//Adds entered player to List
+function addPlayer(){
+    let $newPlayer = $('#playerID').val();   //Grabbing Player ID from entry form
+    playerList.push($newPlayer);             //Adding new Player to PlayerList
+    console.log("New Player Added to list: " + $newPlayer);
+    console.log("Player List: " + playerList);
 }
 
-console.log("index.js imported");
+//Refreshes playerList div
+function refreshPlayerList(){
+    $("#playerList").append( "<br>"+ playerList[(playerList.length - 1)]+ "</br>" );
+    
+}
+
+//Adds player to list + refreshes playerList div
+function addPlayerBtnClick(){
+    addPlayer();
+    refreshPlayerList();
+}
+
+//Randomize Teams
+function randomizeTeams(){
+    //Randomize Array
+
+    //Split Array into 2
+
+    //Display Array to Page
+}
+
+//Load maps to list
+function loadMaps(){
+    
+}
+//Done Button Click
+function done(){ 
+    getSeriesLength();
+}
+
+
