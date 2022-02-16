f30c84745acf43e6078bd235bac3b8d9b9429db1
bmendoza23 <bmendoz2@uwo.ca>
2022-02-16 13:25:59 -0500

    routing static stuff

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const app = express();
 const path = require ('path');
-//app.set("views", path.join(__dirname,"views"));
-
-//Serving Static Contents
+//Serving static contents
 app.use(express.static('static'));
-
+//app.use("/images", express.static(__dirname + '/static/images'));
 app.use(express.urlencoded({
         extended: true
 }));
+
 /*app.get('/', (req,res) => {
         console.log('Rendering Home Page');
         res.status(200).render("index");
